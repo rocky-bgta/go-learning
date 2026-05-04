f7628ff6d44e1a110d5f08a0431b92b9807fdd85
Md. Nazmus Salahin Rocky <rocky.bgta@gmail.com>
2026-05-04 11:37:02 +0600

    Refactor handlers and rendering into separate files, add Bootstrap styling to templates

--- main.go
+++ main.go
@@ -2,31 +2,10 @@ package main
 
 import (
 	"fmt"
-	"html/template"
 	"net/http"
 )
 
 const portNumber = ":8080"
-
-// Home is the home page handler
-func Home(w http.ResponseWriter, r *http.Request) {
-	renderTemplate(w, "home.page.gohtml")
-}
-
-// About is the about page handler
-func About(w http.ResponseWriter, r *http.Request) {
-	renderTemplate(w, "about.page.gohtml")
-}
-
-func renderTemplate(w http.ResponseWriter, tmpl string) {
-	parsedTemplate, _ := template.ParseFiles("./templates/" + tmpl)
-	err := parsedTemplate.Execute(w, nil)
-	if err != nil {
-		fmt.Println("error parsing template:", err)
-		return
-	}
-
-}
 
 // main is the main function
 func main() {
