8637820cc9e4fb45627d8c2dc47b3eece95efc77
Md. Nazmus Salahin Rocky <rocky.bgta@gmail.com>
2026-05-04 11:21:28 +0600

    Replace inline HTML responses with template rendering

--- main.go
+++ main.go
@@ -1,8 +1,8 @@
 package main
 
 import (
-	"errors"
 	"fmt"
+	"html/template"
 	"net/http"
 )
 
@@ -10,50 +10,30 @@ const portNumber = ":8080"
 
 // Home is the home page handler
 func Home(w http.ResponseWriter, r *http.Request) {
-	_, _ = fmt.Fprintf(w, "This is the home page")
+	renderTemplate(w, "home.page.gohtml")
 }
 
 // About is the about page handler
 func About(w http.ResponseWriter, r *http.Request) {
-	sum, _ := addValue(2, 2)
-	_, _ = fmt.Fprintf(w, fmt.Sprintf("The sum of 2 + 2 is %d", sum))
+	renderTemplate(w, "about.page.gohtml")
 }
 
-// addValue adds two integers and return the sum
-func addValue(x, y int) (int, error) {
-	var sum int
-	sum = x + y
-	return sum, nil
+func renderTemplate(w http.ResponseWriter, tmpl string) {
+	parsedTemplate, _ := template.ParseFiles("./templates/" + tmpl)
+	err := parsedTemplate.Execute(w, nil)
+	if err != nil {
+		fmt.Println("error parsing template:", err)
+		return
+	}
+
 }
 
 // main is the main function
 func main() {
-	//fmt.Println("Hello World")
 
 	http.HandleFunc("/", Home)
 	http.HandleFunc("/about", About)
-	http.HandleFunc("/divide", Divide)
 
 	fmt.Println(fmt.Sprintf("Starting application on port %s", portNumber))
 	_ = http.ListenAndServe(portNumber, nil)
-}
-
-func Divide(w http.ResponseWriter, r *http.Request) {
-	f, err := divideValue(100.0, 0.0)
-	if err != nil {
-		fmt.Fprintf(w, "Cannot divide by 0")
-		return
-	}
-
-	fmt.Fprintf(w, fmt.Sprintf("%f divided by %f is %f", 100.0, 10.0, f))
-}
-
-func divideValue(x, y float32) (float32, error) {
-	if y <= 0 {
-		err := errors.New("cannot divide by zero")
-		return 0, err
-	}
-
-	result := x / y
-	return result, nil
 }
