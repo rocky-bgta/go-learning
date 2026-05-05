115db315a64bcda3d02911c758135b616e8a285c
Md. Nazmus Salahin Rocky <rocky.bgta@gmail.com>
2026-05-05 16:10:00 +0600

    Replace pat router with chi router and add Recoverer middleware

--- cmd/web/routes.go
+++ cmd/web/routes.go
@@ -3,17 +3,19 @@ package main
 import (
 	"net/http"
 
-	"github.com/bmizerany/pat"
+	"github.com/go-chi/chi/v5"
+	"github.com/go-chi/chi/v5/middleware"
 	"github.com/rocky_bgta/go-learning/pkg/config"
 	"github.com/rocky_bgta/go-learning/pkg/handlers"
 )
 
 func router(app *config.AppConfig) http.Handler {
-	mux := pat.New()
+	mux := chi.NewRouter()
 
-	mux.Get("/", http.HandlerFunc(handlers.Repo.Home))
-	mux.Get("/about", http.HandlerFunc(handlers.Repo.About))
+	mux.Use(middleware.Recoverer)
 
+	mux.Get("/", handlers.Repo.Home)
+	mux.Get("/about", handlers.Repo.About)
 	return mux
 
 }
